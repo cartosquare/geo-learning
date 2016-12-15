bf5fba1533db39e37bd9bcfe16899f674c374697
XuXiang <xux@geohey.com>
2016-12-15 11:36:52 +0800

    完善随机森林模型

--- geomodel/model_rf.py
+++ geomodel/model_rf.py
@@ -9,22 +9,22 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline, FeatureUnion
 
 
-X_train_file = 'geodata/poi_data/x_train1.pkl'
-y_train_file = 'geodata/poi_data/y_train1.pkl'
-X_test_file = 'geodata/poi_data/x_test1.pkl' 
-y_test_file = 'geodata/poi_data/y_test1.pkl'
+X_train_file = 'geodata/poi_data_smooth/x_train_pois.pkl'
+y_train_file = 'geodata/poi_data_smooth/y_train_pois.pkl'
+X_test_file = 'geodata/poi_data_smooth/x_test_pois.pkl' 
+y_test_file = 'geodata/poi_data_smooth/y_test_pois.pkl'
 
-def mean_absolute_error(y_true, y_pred):
+def mean_absolute_percentage_error(y_true, y_pred):
     # convert pd.series to numpy.ndarray
     y_true = y_true.as_matrix()
 
-    mape = np.abs(y_true - y_pred).sum() / float(y_true.shape[0])
-    
-    print 'mae-cus', mape
+    dim = y_true.shape
+    mape = np.abs((y_true - y_pred) / y_true).sum() / float(dim[0])
+    print 'mape', mape
     return mape
 
-loss_func = make_scorer(mean_absolute_error, greater_is_better=True)
-score_func = make_scorer(mean_absolute_error, greater_is_better=False)
+loss_func = make_scorer(mean_absolute_percentage_error, greater_is_better=True)
+score_func = make_scorer(mean_absolute_percentage_error, greater_is_better=False)
 
 # loading features
 with open(X_train_file, 'rb') as f:
@@ -45,34 +45,34 @@ print 'X test shape: ', X_test.shape
 print 'y test shape: ', y_test.shape
 
 # This dataset is way too high-dimensional. Better do PCA:
-pca = PCA(n_components=7)
+#pca = PCA(n_components=7)
 
 # Maybe some original features where good, too?
-selection = SelectKBest(k=3)
+#selection = SelectKBest(k=3)
 
 # Build estimator from PCA and Univariate selection:
 
-combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
+#combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
 
 # Use combined features to transform dataset:
-X_features = combined_features.fit(X_train, y_train).transform(X_train)
-X_testfeatures = combined_features.transform(X_test)
+#X_features = combined_features.fit(X_train, y_train).transform(X_train)
+#X_testfeatures = combined_features.transform(X_test)
 
-model = RandomForestRegressor(n_estimators=300, max_features=0.6, n_jobs=-1, random_state=42, criterion='mse', min_samples_split=2, max_depth=None, min_samples_leaf=1)
+model = RandomForestRegressor(n_estimators=300, max_features=0.6, n_jobs=-1, random_state=42, min_samples_split=2, max_depth=None, min_samples_leaf=1)
 
 # 使用交叉验证方式训练模型并取得错误率
-scores = cross_validation.cross_val_score(model, X_features, y_train, cv=4, n_jobs=-1, scoring='neg_mean_absolute_error')
+scores = cross_validation.cross_val_score(model, X_train, y_train, cv=4, n_jobs=-1, scoring=score_func)
 
 # 输出交叉验证平均错误率
 mean_score = np.abs(np.mean(scores))
 print('cross validate score is %f' % (mean_score))
 
 # 训练模型
-model.fit(X_features, y_train)
+model.fit(X_train, y_train)
 
 # 预测
-y_pred = model.predict(X_testfeatures)
-error = mean_absolute_error(y_test, y_pred)
+y_pred = model.predict(X_test)
+error = mean_absolute_percentage_error(y_test, y_pred)
 print('predict error %f' % (error))
 
 # feature importance
@@ -80,6 +80,15 @@ importances = model.feature_importances_
 indices = np.argsort(importances)[::-1]
 
 # Print the feature ranking
+brand_names = []
+with open('geodata/poi_scripts/brands.csv') as f:
+    for line in f:
+        name,frequence = line.strip().split(',')
+        brand_names.append(name)
+
+
 print('Feature ranking:')
-for f in range(2):
-    print("%d. feature %s (%f)" % (f + 1, X_train.columns.values[indices[f]], importances[indices[f]]))
+with open('geodata/poi_scripts/best_brands_rf.csv', 'w') as ff:
+    for f in range(10):
+        print("%d. feature %s (%f)" % (f + 1, X_train.columns.values[indices[f]], importances[indices[f]]))
+        ff.write('%d,%s,%s\n' % (X_train.columns.values[indices[f]], brand_names[X_train.columns.values[indices[f]]], importances[indices[f]]))
--- geomodel/turning_rf.py
+++ geomodel/turning_rf.py
@@ -11,33 +11,33 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline, FeatureUnion
 
 
-X_train_file = 'geodata/poi_data/x_train1.pkl'
-y_train_file = 'geodata/poi_data/y_train1.pkl'
-X_test_file = 'geodata/poi_data/x_test1.pkl' 
-y_test_file = 'geodata/poi_data/y_test1.pkl'
+X_train_file = 'geodata/poi_data_smooth/x_train.pkl'
+y_train_file = 'geodata/poi_data_smooth/y_train.pkl'
+X_test_file = 'geodata/poi_data_smooth/x_test.pkl' 
+y_test_file = 'geodata/poi_data_smooth/y_test.pkl'
 
-def mean_absolute_error(y_true, y_pred):
+def mean_absolute_percentage_error(y_true, y_pred):
     # convert pd.series to numpy.ndarray
     y_true = y_true.as_matrix()
 
-    mape = np.abs(y_true - y_pred).sum() / float(y_true.shape[0])
-    
-    print 'mae-cus', mape
+    dim = y_true.shape
+    mape = np.abs((y_true - y_pred) / y_true).sum() / float(dim[0])
+    print 'mape', mape
     return mape
 
-loss_func = make_scorer(mean_absolute_error, greater_is_better=True)
-score_func = make_scorer(mean_absolute_error, greater_is_better=False)
+loss_func = make_scorer(mean_absolute_percentage_error, greater_is_better=True)
+score_func = make_scorer(mean_absolute_percentage_error, greater_is_better=False)
 
 
 def run_model(param):
-    model = RandomForestRegressor(n_estimators=int(param['n_estimators']), max_features=param['max_features'], criterion='mse', n_jobs=-1, random_state=42)
+    model = RandomForestRegressor(n_estimators=int(param['n_estimators']), max_features=param['max_features'], n_jobs=-1, random_state=42)
 
     # 训练模型
-    model.fit(X_features, y_train)
+    model.fit(X_train, y_train)
 
     # 预测
-    y_pred = model.predict(X_testfeatures)
-    error = mean_absolute_error(y_test, y_pred)
+    y_pred = model.predict(X_test)
+    error = mean_absolute_percentage_error(y_test, y_pred)
     print('predict error %f' % (error))
 
     # feature importance
@@ -45,15 +45,24 @@ def run_model(param):
     indices = np.argsort(importances)[::-1]
 
     # Print the feature ranking
+    brand_names = []
+    with open('geodata/poi_scripts/brands.csv') as f:
+        for line in f:
+            name,frequence = line.strip().split(',')
+            brand_names.append(name)
+
+
     print('Feature ranking:')
-    for f in range(2):
-        print("%d. feature %s (%f)" % (f + 1, X_train.columns.values[indices[f]], importances[indices[f]]))
+    with open('geodata/poi_scripts/best_brands_rf.csv', 'w') as ff:
+        for f in range(10):
+            print("%d. feature %s (%f)" % (f + 1, X_train.columns.values[indices[f]], importances[indices[f]]))
+            ff.write('%d,%s,%s\n' % (X_train.columns.values[indices[f]], brand_names[X_train.columns.values[indices[f]]], importances[indices[f]]))
 
 
 def score(param):
-    model = RandomForestRegressor(n_estimators=int(param['n_estimators']), max_features=param['max_features'], criterion='mse', n_jobs=-1, random_state=42)
+    model = RandomForestRegressor(n_estimators=int(param['n_estimators']), max_features=param['max_features'], n_jobs=-1, random_state=42)
 
-    scores = cross_validation.cross_val_score(model, X_features, y_train, cv=4, n_jobs=-1, scoring='neg_mean_absolute_error')
+    scores = cross_validation.cross_val_score(model, X_train, y_train, cv=4, n_jobs=-1, scoring=score_func)
 
     # 输出交叉验证平均错误率
     mean_score = np.abs(np.mean(scores))
@@ -66,7 +75,7 @@ def optimize(trials):
         'n_estimators': hp.quniform("n_estimators", 10, 10000, 10),
         'max_features': hp.quniform("max_features", 0.05, 1.0, 0.05)
     }
-    best = fmin(score, space, algo=tpe.suggest, trials=trials, max_evals=100)
+    best = fmin(score, space, algo=tpe.suggest, trials=trials, max_evals=50)
 
     return best
 
@@ -89,18 +98,18 @@ print 'X test shape: ', X_test.shape
 print 'y test shape: ', y_test.shape
 
 # This dataset is way too high-dimensional. Better do PCA:
-pca = PCA(n_components=7)
+#pca = PCA(n_components=7)
 
 # Maybe some original features where good, too?
-selection = SelectKBest(k=3)
+#selection = SelectKBest(k=3)
 
 # Build estimator from PCA and Univariate selection:
 
-combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
+#combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
 
 # Use combined features to transform dataset:
-X_features = combined_features.fit(X_train, y_train).transform(X_train)
-X_testfeatures = combined_features.transform(X_test)
+#X_features = combined_features.fit(X_train, y_train).transform(X_train)
+#X_testfeatures = combined_features.transform(X_test)
 
 
 # Trials object where the history of search will be stored
